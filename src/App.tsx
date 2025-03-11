200ae044e24b9147379da1c1b849bebfdd20f4bc
USER <sithiraroneth@gmail.com>
2025-03-11 15:34:01 +0530

    add loader

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import './App.css';
+import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import { useState, useEffect } from "react";
 import RootLayout from "./component/RootLayout.tsx";
 import Home from "./pages/Home.tsx";
 import About from "./pages/About.tsx";
 import Works from "./pages/Works.tsx";
 import Contact from "./pages/Contact.tsx";
+import Loader from "./component/Loader.tsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <RootLayout />,
+        children: [
+            { path: "/", element: <Home /> },
+            { path: "/home", element: <Home /> },
+            { path: "/about", element: <About /> },
+            { path: "/works", element: <Works /> },
+            { path: "/contact", element: <Contact /> },
+        ],
+    },
+]);
 
 function App() {
-    const routes = createBrowserRouter([
-        {
-            path: "/",
-            element: <RootLayout />,
-            children: [
-                { path: "/", element: <Home /> },
-                { path: "/home", element: <Home /> },
-                { path: "/about", element: <About /> },
-                { path: "/works", element: <Works /> },
-                { path: "/contact", element: <Contact /> },
-            ]
-        }
-    ]);
-    return <RouterProvider router={routes} />;
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false);
+        }, 2000);
+    }, []);
+
+    return loading ? <Loader /> : <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/component/Loader.tsx
@@ -0,0 +1,8 @@
+import '../component/Loader.css'
+export default function Loader() {
+    return (
+        <div className="LoaderContainer">
+            <div className="Loader"></div>
+        </div>
+    );
+}
