1ac1b1843174fab0b50658819aae951d997a9754
USER <sithiraroneth@gmail.com>
2025-03-11 15:54:29 +0530

    add background animation

---
+++ src/component/Background.tsx
@@ -0,0 +1,12 @@
+import '../component/background.css'
+export default function Background() {
+    return (
+        <div className="area">
+            <ul className="circles">
+                {Array.from({length: 10}).map((_, index) => (
+                    <li key={index}></li>
+                ))}
+            </ul>
+        </div>
+    )
+}
--- src/component/RootLayout.tsx
+++ src/component/RootLayout.tsx
@@ -1,9 +1,11 @@
 import Navigation from "./Navigation.tsx";
 import {Outlet} from "react-router";
+import Background from "./Background.tsx";
 
 function RootLayout(){
     return(
         <>
+            <Background/>
             <Navigation></Navigation>
             <Outlet></Outlet>
         </>
